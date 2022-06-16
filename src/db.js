4e8406c0dd0fdc977b227b4053b9db67a48568f8
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-16 12:13:14 -0300

    db name proyectoFinal

--- src/db.js
+++ src/db.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
-} = process.env;
+} = process.env; 
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/proyectoFinal`, {
   logging: false, // set to console.log to see the raw SQL queries
