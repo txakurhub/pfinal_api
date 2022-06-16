231d3f9cb861753c65f449bf59408e2ade516df1
thomasHeitFux <thomas41392@gmail.com>
2022-06-16 12:23:34 -0300

    testeando

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const axios = require("axios");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +8,30 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+
+
+// const options = {
+//   method: 'GET',
+//   url: 'https://v1-sneakers.p.rapidapi.com/v1/sneakers',
+//   params: {limit: '<REQUIRED>'},
+//   headers: {
+//     'X-RapidAPI-Key': 'fb9d440a17mshdeb12f5ec33d353p1c958bjsn0ce8f2b9c97f',
+//     'X-RapidAPI-Host': 'v1-sneakers.p.rapidapi.com'
+//   }
+// };
+
+
+
+router.get('/sneakers', async (req, res) => {
+    // axios.request(options).then(function (response) {
+    //     res.status(200).send(response.data);
+    // }).catch(function (error) {
+    //     console.error(error);
+    //     res.status(404)
+    // });
+    res.send("holliwish")
+});
 
 
 module.exports = router;
