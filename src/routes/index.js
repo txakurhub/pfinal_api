a32e76c28290910eca29db614dfa5b432256399e
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-06-20 13:56:40 -0300

    prueba

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,27 +10,27 @@ const router = Router();
 
 router.get('/shoes', async (req, res) => {
 
-//--------------------------------------------------------------------------------------------
-const getPepes = async()=>{
-    const pepes=await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=calzado&offset=${page}&limit=5`)
+  //--------------------------------------------------------------------------------------------
+  const getPepes = async () => {
 
-    const result ={
-        // PruebaLink:pepes.data.results.map(e=>e.seller.permalink),
-        // PruebaId:pepes.data.results.map(e=>e.seller.id)
-        title:pepes.data.results.map(e=>e.title),
-        img:pepes.data.results.map(e=>e.thumbnail),
-        brand:pepes.data.results.map(e=>e.attributes[0].value_name),
-        model:pepes.data.results.map(e=>e.attributes[2].value_name),
-        price:pepes.data.results.map(e=>e.price)
+    const pepes = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=zapatillas&offset=${page}&limit=5`)
 
+    const result = {
+      // PruebaLink:pepes.data.results.map(e=>e.seller.permalink),
+      // PruebaId:pepes.data.results.map(e=>e.seller.id)
+      title: pepes.data.results.map(e => e.title),
+      img: pepes.data.results.map(e => e.thumbnail),
+      brand: pepes.data.results.map(e => e.attributes[0].value_name),
+      model: pepes.data.results.map(e => e.attributes[2].value_name),
+      price: pepes.data.results.map(e => e.price)
     }
     return result
-}
-//----------------------------------------------------------------------------------------------
+  }
+  //----------------------------------------------------------------------------------------------
 
-    const page=req.query.page||0;
-    let pepes = await getPepes()
-    res.json(pepes)
+  const page = req.query.page || 0;
+  let pepes = await getPepes()
+  res.status(200).json(pepes)
 });
 
 
