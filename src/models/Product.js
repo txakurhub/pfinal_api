234aeff8ec9f97c89aaaa5efd1e4d60cd50a7772
LucianoScaglione <lucianoscaglione21@gmail.com>
2022-06-20 17:06:46 -0300

    Rutas back

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,6 +2,11 @@ const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
   sequelize.define('Product', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    },
     title: {
       type: DataTypes.STRING
     },
