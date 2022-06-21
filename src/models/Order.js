d39e4eea24e259265e4cdfa0acd8d7d04a5302c3
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-21 10:14:20 -0300

    agregados review y wishlist a models y db + models identados con prettier

--- src/models/Order.js
+++ src/models/Order.js
@@ -1,25 +1,29 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-  sequelize.define('Order', {
-    amount: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
+  sequelize.define(
+    "Order",
+    {
+      amount: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      shipping_address: {
+        type: DataTypes.TEXT,
+      },
+      order_address: {
+        type: DataTypes.TEXT,
+      },
+      order_email: {
+        type: DataTypes.STRING,
+      },
+      order_date: {
+        type: DataTypes.DATE,
+      },
+      order_status: {
+        type: DataTypes.STRING,
+      },
     },
-    shipping_address: {
-      type: DataTypes.TEXT
-    },
-    order_address: {
-      type: DataTypes.TEXT
-    },
-    order_email: {
-      type: DataTypes.STRING
-    },
-    order_date: {
-      type: DataTypes.DATE
-    },
-    order_status: {
-      type: DataTypes.STRING
-    }
-  }, { timestamps: false });
+    { timestamps: false }
+  );
 };
