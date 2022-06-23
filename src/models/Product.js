8859a053beaf94252a67bd5e40d7076e80b77dd2
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-22 21:07:45 -0300

    unique true en model product

--- src/models/Product.js
+++ src/models/Product.js
@@ -8,6 +8,7 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         allowNull: false,
         primaryKey: true,
+        unique: true
       },
       title: {
         type: DataTypes.STRING,
