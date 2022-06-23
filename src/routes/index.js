7f7a6425e34b19316476d6554e0678b3161c6a82
fvaron69 <fredyalberbaron@hotmail.com>
2022-06-23 09:23:48 -0500

    wishlist

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,12 +5,14 @@ const shoesRouter = require("./shoes");
 const customersRouter = require("./customer")
 const categoriesRouter = require("./categories")
 const reviewsRouter = require("./review")
+const wishlistRouter = require("./wishlist")
 //--------------------------ROUTES
 
 router.use("/shoes", shoesRouter);
 router.use("/categories", categoriesRouter);
 router.use("/customers", customersRouter);
 router.use("/reviews", reviewsRouter);
+router.use("/wishlist", wishlistRouter)
 
 
 module.exports = router;
