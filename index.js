1360bc40d3289d601b1912ad9d75f06e7daf9e33
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-26 21:18:43 -0300

    mods para deploy

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const server = require("./src/app.js");
+const { conn } = require("./src/db.js");
+const PORT = process.env;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
-    console.log('Servidor corriendo en el puerto 3001'); 
+  server.listen(PORT, () => {
+    console.log("Servidor corriendo en el puerto " + PORT);
   });
 });
