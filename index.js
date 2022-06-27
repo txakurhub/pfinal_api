becb94a9a2a885ed4c90315b836742c46ac90b0b
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-26 21:36:02 -0300

    cors destructurado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const PORT = process.env;
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(PORT, () => {
     console.log("Servidor corriendo en el puerto " + PORT); 
   });
