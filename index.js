31dc121d488e8b38cba5ca89509b56b59f03b237
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-26 21:21:10 -0300

    pull

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const PORT = process.env;
 
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
-    console.log("Servidor corriendo en el puerto " + PORT);
+    console.log("Servidor corriendo en el puerto " + PORT); 
   });
 });
