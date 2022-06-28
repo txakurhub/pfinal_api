5ef0e954598f812fccebbd7b03f8fdb4e9f6a9fd
Leandro Pereyra <me.leandrop@gmail.com>
2022-06-28 18:33:54 -0300

    user con firebase y modificaciones random

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 const { PRODUCTION, PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(PRODUCTION ? PORT : 3001, () => {
+  server.listen(PRODUCTION ? 3001 : PORT, () => {
     console.log("Servidor corriendo en el puerto 3001");
   });
 });
