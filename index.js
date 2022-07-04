a0b7737bed06117ea6b5da5bbb752c5a76677456
Leandro Pereyra <me.leandrop@gmail.com>
2022-07-03 22:22:16 -0300

    vamo heroku

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db.js");
 
 conn.sync({ force: true }).then(() => {
   server.listen(process.env.PORT, () => {
-    console.log("Servidor corriendo en el puerto " + PORT);
+    console.log("Servidor corriendo en el puerto " + process.env.PORT);
   });
 });
  
