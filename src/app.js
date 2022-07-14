4c0cd1c7ed789b4492637d7dd90735ba346caf58
Leandro Pereyra <70654398+txakurhub@users.noreply.github.com>
2022-07-14 03:10:20 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
-const URL_LOCAL = "http://localhost:3000";
-const { CORS_URL, PRODUCTION } = process.env;
+const { CORS_URL } = process.env;
+
 require("./db.js");
 
 const server = express();
@@ -16,7 +16,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", PRODUCTION ? URL_LOCAL : CORS_URL); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", CORS_URL); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
