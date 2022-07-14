7c9d93b20c5dae3a4f35bfc7b9133c6745ec3dd4
Leandro Pereyra <70654398+txakurhub@users.noreply.github.com>
2022-07-14 13:24:29 -0300

    Update payments.js

--- src/routes/payments.js
+++ src/routes/payments.js
@@ -1,54 +1,69 @@
 const { Router } = require("express");
 const router = Router();
 const axios = require("axios");
-const URL = "https://zapatillas-proyecto.herokuapp.com";
+const { PRODUCTION, CORS_URL } = process.env;
+const LOCAL_BACK = "http://localhost:3001";
+const URL_DEPLOY = "https://zapatillas-proyecto.herokuapp.com";
 
 router.post("/", async (req, res) => {
   const { items, email, user_id } = req.body;
 
   try {
-    const result = await createPayment(items);
-    await axios.post(`${URL}/order`, { user_id, email, items });
-    res.status(200).send(result);
+    console.log(PRODUCTION)
+    const orden = (await axios.post(`${PRODUCTION ? LOCAL_BACK : URL_DEPLOY}/order`, { user_id, email, items })).data;
+    const result = await createPayment(items, orden.id);
+    res.send(result);
   } catch (error) {
     console.log("Entra aca", error);
     res.status(404).send({ error: error.message });
   }
 });
 
-router.get("/success", (req, res) => {
+router.get("/success/:id", async (req, res) => {
+
+  const {id} = req.params;
+
   try {
-    res.status(200).send("Se completo el pago con exito");
+    await axios.put(`${PRODUCTION ? LOCAL_BACK : URL_DEPLOY}/order/${id}`, {order: "realizada"})
+    res.redirect(PRODUCTION ? "http://localhost:3000/" : CORS_URL);
   } catch (error) {
     res.send({ error: error.message });
   }
 });
 
-router.get("/failure", (req, res) => {
+router.get("/failure/:id", async(req, res) => {
+
+  const {id} = req.params;
+
   try {
-    res.status(200).send("No se pudo completar el pago");
+    await axios.put(`${PRODUCTION ? LOCAL_BACK : URL_DEPLOY}/order/${id}`, {order: "cancelada"})
+    res.redirect(PRODUCTION ? "http://localhost:3000/" : CORS_URL);
   } catch (error) {
-    res.status(400).send({ error: error.message });
+    res.send({ error: error.message });
   }
 });
 
-router.get("/pending", (req, res) => {
+router.get("/pending/:id", async (req, res) => {
+
+  const {id} = req.params;
+
   try {
-    res.status(200).send("El pago esta pendiente");
+    await axios.put(`${PRODUCTION ? LOCAL_BACK : URL_DEPLOY}/order/${id}`, {order: "pendiente"})
+    res.redirect(PRODUCTION ? "http://localhost:3000/" : CORS_URL);
   } catch (error) {
-    res.status(400).send({ error: error.message });
+    res.send({ error: error.message });
   }
 });
 
-async function createPayment(item) {
+async function createPayment(item, id) {
   const url = "https://api.mercadopago.com/checkout/preferences";
   const body = {
     items: item,
     back_urls: {
-      failure: "/failure",
-      pending: "/pending",
-      success: "/success",
-    },
+      failure: `${PRODUCTION ? LOCAL_BACK : URL_DEPLOY}/payments/failure/${id}`,
+      pending: `${PRODUCTION ? LOCAL_BACK : URL_DEPLOY}/payments/pending/${id}`,
+      success: `${PRODUCTION ? LOCAL_BACK : URL_DEPLOY}/payments/success/${id}`,
+    }
   };
   const payment = await axios.post(url, body, {
     headers: {
@@ -56,16 +71,17 @@ async function createPayment(item) {
       Authorization: `Bearer ${process.env.ACCES_TOKEN}`,
     },
   });
-  ////ACCES_TOKEN = APP_USR-7186342339590293-051403-1dd7693603cbe79be81d357b18b1a2cc-185162521
+////ACCES_TOKEN = APP_USR-7186342339590293-051403-1dd7693603cbe79be81d357b18b1a2cc-185162521
 
-  const result = [
-    payment.data.init_point,
-    payment.data.items.map((e) => {
+    const result = [
+      payment.data.init_point,
+      payment.data.id,
+      payment.data.items.map((e) => {
       return e;
-    }),
-  ];
-  console.log("Es result ", result);
-  return result;
+      }),
+    ];
+    console.log("Esto es payments ", payment);
+    return result;
 }
 
 module.exports = router;
