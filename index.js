80508d350811a16e37975a95e6db9568eb02bb2d
Leandro Pereyra <70654398+txakurhub@users.noreply.github.com>
2022-07-14 03:09:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-const { PRODUCTION, PORT } = process.env;
 
 conn.sync({ force: true }).then(() => {
-  server.listen(PRODUCTION ? 3001 : PORT, () => {
-    console.log("Servidor corriendo en el puerto 3001");
+  server.listen(process.env.PORT, () => {
+    console.log("Servidor corriendo en el puerto " + process.env.PORT);
   });
 });
+ 
